9ac85cfcb3f6e31e55f0190a547c10808604e2ab
ilba147 <ilba14@icloud.com>
2025-12-19 12:34:05 +0300

    Create pr11 and pr13.cpp

---
+++ pr11 and pr13.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int choice;
+    int type;
+    int size;
+    char texture;
+
+    cout << "Программа - \"Геометрические фигуры\"\n\n";
+    cout << "1) Линия\n";
+    cout << "2) Квадрат\n\n";
+    cout << "Выберите фигуру: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "\n\nФигура: \"Линия\"\n\n";
+        cout << "\nРазмер: ";
+        cin >> size;
+        cout << "Текстура: ";
+        cin >> texture;
+
+
+        for (int i = 0; i < size; i++) {
+            cout << texture << " ";
+        }
+        cout << "\n";
+    }
+
+
+
+
+    else if (choice == 2) {
+        cout << "\n\nФигура: \"Квадрат\"\n\n";
+        cout << "1) Заполненный\n";
+        cout << "2) Пустой\n\n";
+        cout << "Выберите тип: ";
+        cin >> type;
+
+        cout << "\nРазмер: ";
+        cin >> size;
+        cout << "Текстура: ";
+        cin >> texture;
+
+        cout << "\n\nРезультат:\n\n";
+
+        if (type == 1) {
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    cout << texture << " ";
+                }
+                cout << "\n";
+            }
+        }
+        else if (type == 2) {
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                        cout << texture << " ";
+                    }
+                    else {
+                        cout << "  ";
+                    }
+                }
+                cout << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
+        
