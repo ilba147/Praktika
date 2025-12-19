9559b1e181ed12a12945ca6c70905da73926a66c
ilba147 <ilba14@icloud.com>
2025-12-19 12:51:13 +0300

    Update pr11 and pr13.cpp

--- pr11 and pr13.cpp
+++ pr11 and pr13.cpp
@@ -6,6 +6,7 @@ int main() {
     int choice;
     int type;
     int size;
+    int shir;
     char texture;
 
     cout << "Программа - \"Геометрические фигуры\"\n\n";
@@ -60,6 +61,45 @@ int main() {
                 for (int j = 0; j < size; j++) {
                     if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
                         cout << texture << " ";
+                    }
+                    else {
+                        cout << "  ";
+                    }
+                }
+                cout << "\n";
+            }
+        }
+    }
+    else if (choice == 3) {
+        cout << "\n\nФигура: \"Прямогуольник\"\n\n";
+        cout << "1) Заполненный\n";
+        cout << "2) Пустой\n\n";
+        cout << "Выберите тип: ";
+        cin >> type;
+
+        cout << "\nВысота: ";
+        cin >> size;
+        cout << "\nШирина: ";
+        cin >> shir;
+        cout << "\nТекстура: ";
+        cin >> texture;
+
+        cout << "\n\nРезультат:\n\n";
+
+        if (type == 1) {
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < shir; j++) {
+                    cout << texture << " ";
+                }
+                cout << "\n";
+            }
+        }
+    else if (type == 2) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < shir; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == shir - 1) {
+                    cout << texture << " ";
                     }
                     else {
                         cout << "  ";
